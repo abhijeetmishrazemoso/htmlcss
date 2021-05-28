2017ee82a633925a568183e3f76a8622a72eb7d6
Abhijeet Mishra <abhijeet.mishra@zemosolabs.com>
2021-05-28 19:53:38 +0530

    segregated files

---
+++ js/flexJavascript.js
@@ -0,0 +1,33 @@
+
+        function activate(activeId,idOfSourceButton){
+            var tabs = document.getElementsByClassName("tabbing");
+            var tabButtons = document.getElementsByClassName('tabs');
+            for (let i=0;i<tabs.length;i++) {
+                tabs[i].style.display = "none";
+            }
+            for(let i=0;i<tabButtons.length;i++){
+                tabButtons[i].style.backgroundColor = 'lightgrey';
+            }
+            document.getElementById(activeId).style.display = "flex";
+            document.getElementById(idOfSourceButton).style.backgroundColor = 'white'; 
+            document.getElementById(idOfSourceButton).style.color = 'grey'; 
+        }
+        function sendConfirmation(){
+            
+            var formm = document.forms[0];
+            formm.elements.Subj.value ="";
+            formm.elements.Message.value = "";  
+            document.getElementById("resultMessage").innerHTML = "Message Sent!";
+            document.getElementById("resultMessageh2").innerHTML = "Message Sent!";
+            
+            alert("Message Sent!");
+            changeMessageByTimeOut();
+            return false;
+        }
+
+        function changeMessageByTimeOut(){
+                setTimeout(() => {
+                    document.getElementById("resultMessage").innerHTML = "Waiting for a new Message!";
+                    document.getElementById("resultMessageh2").innerHTML = "Waiting for a new Message!";
+                }, 4000);
+        }
